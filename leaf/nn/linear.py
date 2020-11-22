2037d2b99a728a48548d55ddd1c27dc2b48db52d
yngtodd <young.todd.mk@gmail.com>
2020-11-22 11:54:50 -0500

    Fix super initialization
    This should refer to the child class, not the parent class.

--- leaf/nn/linear.py
+++ leaf/nn/linear.py
@@ -12,7 +12,7 @@ class Linear(Module):
     """
 
     def __init__(self, n_inputs, n_neurons):
-        super(Module, self).__init__()
+        super(Linear, self).__init__()
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
