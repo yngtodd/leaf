82fae3e6753f0c28aab51c598df02a3a591db6dc
yngtodd <young.todd.mk@gmail.com>
2020-11-25 00:08:29 -0500

    Fix softmax

--- leaf/nn/functional.py
+++ leaf/nn/functional.py
@@ -8,5 +8,6 @@ def relu(x):
 
 def softmax(x, dim=1):
     f"""Softmax activation function"""
-    exp = np.exp(x)
-    return exp / np.sum(exp, axis=dim, keepdims=True)
+    exp = np.exp(x, np.max(x, axis=dim, keepdims=True))
+    probabilities = exp / np.sum(exp, axis=dim, keepdims=True)
+    return probabilities
